a7fc78bcfea82ebbc064d9c1374f8140d5dcf059
Maros Kollar <maros@k-1.com>
2017-10-17 08:21:47 +0200

    Fix Koelner Phonetik #123300
    Fix handing of anlaut and C special rules

--- lib/Text/Phonetic/Koeln.pm
+++ lib/Text/Phonetic/Koeln.pm
@@ -17,10 +17,10 @@ sub _do_encode {
 
     # Replace umlaut
     $string =~ s/ß/S/g;
-    $string =~ s/[äÄ]/AE/g;
-    $string =~ s/[öÖ]/OE/g;
-    $string =~ s/[üÜ]/UE/g;
-    
+    $string =~ s/Ä/AE/g;
+    $string =~ s/Ö/OE/g;
+    $string =~ s/Ü/UE/g;
+
     # Replace double consonants
     #$string =~ s/([BCDFGHJKLMNPQRSTVWXZ])\1+/$1/g;
 
@@ -29,11 +29,7 @@ sub _do_encode {
     $result = '';
 
     # Handle initial sounds
-    # A,E,I,J,O,U,Y => 0
-    if (Text::Phonetic::_is_inlist($character_list[0],qw(A E I J Y O U))) {
-        $result .= 0;
-        $last_match = shift @character_list;
-    } elsif ($character_list[0] eq 'C') {
+    if ($character_list[0] eq 'C') {
         if (Text::Phonetic::_is_inlist($character_list[1],qw(A H K L O Q R U X))) {
             $result .= 4;
         } else {
@@ -41,115 +37,94 @@ sub _do_encode {
         }
         $last_match = shift @character_list;
     }
-    
+
     # Loop all characters
     while (scalar(@character_list)) {
         # A,E,I,J,O,U,Y => 0
-        #if (Text::Phonetic::_is_inlist($character_list[0],qw(A E I J O U Y))) {
-        #    $result .= 0;
-        #    $last_match = shift @character_list;
-        #    next;
-        #}
+        if (Text::Phonetic::_is_inlist($character_list[0],qw(A E I J Y O U))) {
+            $result .= 0;
+            $last_match = shift @character_list;
         # B => 1
-        if ($character_list[0] eq 'B') {
+        } elsif ($character_list[0] eq 'B') {
             $result .= 1;
             $last_match = shift @character_list;
-            next;
-        }
         # P in front of H => 1
         # P => 3
-        if ($character_list[0] eq 'P') {
+        } elsif ($character_list[0] eq 'P') {
             if (defined($character_list[1])
-                && $character_list[1] eq 'H') {    
-                $result .= 3;    
+                && $character_list[1] eq 'H') {
+                $result .= 3;
             } else {
                 $result .= 1;
             }
             $last_match = shift @character_list;
-            next;
-        }
         # D,T in front of C,S,Z => 8
         # D,T => 2
-        if (Text::Phonetic::_is_inlist($character_list[0],qw(D T))) {
+        } elsif (Text::Phonetic::_is_inlist($character_list[0],qw(D T))) {
             if (defined($character_list[1]) && $character_list[1] =~ m/[CSZ]/) {
                 $result .= 8;
             } else {
                 $result .= 2;
             }
             $last_match = shift @character_list;
-            next;
-        }
         # F,V,W => 3
-        if (Text::Phonetic::_is_inlist($character_list[0],qw(F V W))) {
+        } elsif (Text::Phonetic::_is_inlist($character_list[0],qw(F V W))) {
             $result .= 3;
             $last_match = shift @character_list;
-            next;
-        } 
         # C in front of A,H,K,O,Q,U,X => 4
         # C after S,Z => 8
-        if ($character_list[0] eq 'C') {
+        } elsif ($character_list[0] eq 'C') {
             if (Text::Phonetic::_is_inlist($last_match,qw(S Z))) {
                 $result .= 8;
             } elsif (defined($character_list[1])
-                && Text::Phonetic::_is_inlist($character_list[1],qw(A H K O Q U X))) {    
+                && Text::Phonetic::_is_inlist($character_list[1],qw(A H K O Q U X))) {
                 $result .= 4;
+            } else {
+                $result .= 8;
             }
             $last_match = shift @character_list;
-            next;
-        }
-        
         # G,K,Q => 4
-        if (Text::Phonetic::_is_inlist($character_list[0],qw(G Q K))) {
+        } elsif (Text::Phonetic::_is_inlist($character_list[0],qw(G Q K))) {
             $result .= 4;
             $last_match = shift @character_list;
-            next;
-        }
         # X not after C,K,Q => 48
         # X after C,K,Q => 8
-        if ($character_list[0] eq 'X') {
+        } elsif ($character_list[0] eq 'X') {
             if (Text::Phonetic::_is_inlist($last_match,qw(C K Q))) {
                 $result .= 8;
             } else {
                 $result .= 48;
             }
             $last_match = shift @character_list;
-            next;
-        }
         # L => 5
-        if ($character_list[0] eq 'L') {
+        } elsif ($character_list[0] eq 'L') {
             $result .= 5;
             $last_match = shift @character_list;
-            next;
-        }
         # M,N => 6
-        if (Text::Phonetic::_is_inlist($character_list[0],qw(M N))) {
+        } elsif (Text::Phonetic::_is_inlist($character_list[0],qw(M N))) {
             $result .= 6;
             $last_match = shift @character_list;
-            next;
-        }    
         # R => 7
-        if ($character_list[0] eq 'R') {
+        } elsif ($character_list[0] eq 'R') {
             $result .= 7;
             $last_match = shift @character_list;
-            next;
-        }
         # S,Z => 8
-        if (Text::Phonetic::_is_inlist($character_list[0],qw(S Z))) {
+        } elsif (Text::Phonetic::_is_inlist($character_list[0],qw(S Z))) {
             $result .= 8;
             $last_match = shift @character_list;
-            next;
+        # No rule matched
+        } else {
+            $last_match = shift @character_list;
         }
 
-        # No rule matched
-        $last_match = shift @character_list;
     }
-    
+
     # Replace consecutive codes
     $result =~ s/(\d)\1+/$1/g;
-    
+
     # Replace zero code (except for first position)
-    #$result =~ s/(\d+)0/$1/g;
-    
+    $result =~ s/([1-9])0+/$1/g;
+
     return $result
 }
 
